1e5d7d4323b4d2bdfb067d618a492633cc358f3c
Jonas Rodrigues <jonasrodrigues2009@gmail.com>
2015-12-09 11:35:52 +0000

    Initial Commit

---
+++ binary_search_v2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  binary_search_v2
+//
+//  Created by Jonas Rodrigues on 09/12/15.
+//  Copyright © 2015 Jonas Rodrigues. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
